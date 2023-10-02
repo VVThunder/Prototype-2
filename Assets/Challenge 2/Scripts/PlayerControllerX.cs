0326d636dafb4b9b7f748b003d233b22898add17
VVThunder <145505541+VVThunder@users.noreply.github.com>
2023-10-02 16:58:00 +0100

    Dog Spawn Timer Fix
    Challenge complete and the player can no longer spam spacebar.

--- Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
+    private float spawnCooldown = 0;
 
     // Update is called once per frame
     void Update()
     {
         // On spacebar press, send dog
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && spawnCooldown <= 0)
         {
             Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            spawnCooldown = 1;
+        }
+        if (spawnCooldown >= 0)
+        {
+            spawnCooldown -= Time.deltaTime;
         }
     }
 }
